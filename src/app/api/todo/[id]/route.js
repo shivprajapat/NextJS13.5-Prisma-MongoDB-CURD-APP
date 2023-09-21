c3dce835561260773d9af80dc1a1f7dc1f2c1d38
yudiz-shiv <shivkumar.prajapat@yudiz.com>
2023-09-21 12:11:32 +0530

    feat: 🎸 prisma singl id delete api

--- src/app/api/todo/[id]/route.js
+++ src/app/api/todo/[id]/route.js
@@ -19,3 +19,50 @@ export const GET = async (request, { params }) => {
     return NextResponse({ message: "get post error", error }, { status: 500 });
   }
 };
+
+// PATCH Single Request
+
+export const PATCH = async (request, { params }) => {
+  try {
+    const body = await request.json();
+    const { id } = params;
+    const { title, description } = body;
+
+    const updatePost = await prisma.post.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        description,
+      },
+    });
+    if (!updatePost) {
+      return NextResponse.json({ message: "post not found" }, { status: 404 });
+    }
+    return NextResponse.json(updatePost);
+  } catch (error) {
+    return NextResponse(
+      { message: "update post error", error },
+      { status: 500 }
+    );
+  }
+};
+
+// DELETE Single Request
+export const DELETE = async (request, { params }) => {
+  try {
+    const { id } = params;
+    const updatePost = await prisma.post.delete({
+      where: {
+        id,
+      },
+    });
+    return NextResponse.json({ message: "your data has been deleted!" });
+  } catch (error) {
+    return NextResponse(
+      { message: "post delete error", error },
+      { status: 500 }
+    );
+  }
+};
